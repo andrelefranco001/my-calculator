a01dde023cd725105bb6380137af809b766810f0
andres_velasquez01 <andrelefranco001@gmail.com>
2023-05-23 21:22:27 -0500

    Deploy app

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import mylogo from './assets/mylogo1.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <img
           className="logomy animate__animated animate__fadeIn"
-          src= '../public/mylogo1.png'
+          src= {mylogo} 
           alt="logo"
         />
     <h3 className="animate__animated animate__fadeIn"> My Technology is created by Andres Velasquez Franco</h3>
